82518693df2765fc4eb92e26e4089a284ab4ca1a
hosein.sharifinia <hosein.sharifinia@gmail.com>
2020-06-14 12:03:58 +0430

    add keyboard component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
+import "react-simple-keyboard/build/css/index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
